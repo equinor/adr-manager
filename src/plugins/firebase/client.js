e3be3cad129f07952354752f759fe4970ae7e8cc
Hjalmar Otto Fjøsne <hjaf@equinor.com>
2024-05-29 13:57:17 +0000

    Remove firebaseConfig (came with fork)

--- src/plugins/firebase/client.js
+++ src/plugins/firebase/client.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GithubAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDLeDiHmseMx-w9Ba7TI4lL8Lvc0M7Zpeg",
-    authDomain: "adr-manager.firebaseapp.com",
-    projectId: "adr-manager",
-    storageBucket: "adr-manager.appspot.com",
-    messagingSenderId: "313525337788",
-    appId: "1:313525337788:web:0834a7a168fe39352adab9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
